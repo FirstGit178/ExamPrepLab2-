f2d8aae96f33bd13d4358d32a41c85ffcbd698f6
Fergal McNamara <g00180082@atu.ie>
2025-11-18 13:33:42 +0000

    Setting up Springboot project

---
+++ src/main/java/ie/atu/exampreplab2/ExamPrepLab2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.exampreplab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrepLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPrepLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/exampreplab2/ExamPrepLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.exampreplab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPrepLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
